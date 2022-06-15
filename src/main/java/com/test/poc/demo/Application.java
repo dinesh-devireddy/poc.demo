2cf9c856523552824c13f51fc2db68bd5cd8e6cf
dineshkumar9 <dineshdevireddy927@gmail.com>
2022-06-15 21:59:59 +0530

    added CODE to zip file

--- src/main/java/com/test/poc/demo/Application.java
+++ src/main/java/com/test/poc/demo/Application.java
@@ -2,9 +2,15 @@ package com.test.poc.demo;
 
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -94,7 +100,38 @@ public void createExcel() throws IOException {
 		
 		try (FileOutputStream saveExcel = new FileOutputStream("target/testDinesh.xlsx")) {
 		    workbook.write(saveExcel);
+		    
+		   
 		}
+		/*
+		 * ClassLoader classLoader = getClass().getClassLoader(); String path =
+		 * classLoader.getResource("testDinesh.xlsx").getPath(); zipFile(path);
+		 */
+		zipFile("target/testDinesh.xlsx");
+		 System.out.println("target/testDinesh.xlsx");
 	}
+	
+	private static void zipFile(String filePath) {
+        try {
+            File file = new File(filePath);
+            //String zipFileName = file.getName().concat(".zip");
+            String zipFileName = "testDinesh.zip";
+ 
+            FileOutputStream fos = new FileOutputStream(zipFileName);
+            ZipOutputStream zos = new ZipOutputStream(fos);
+ 
+            zos.putNextEntry(new ZipEntry(file.getName()));
+ 
+            byte[] bytes = Files.readAllBytes(Paths.get(filePath));
+            zos.write(bytes, 0, bytes.length);
+            zos.closeEntry();
+            zos.close();
+ 
+        } catch (FileNotFoundException ex) {
+            System.err.format("The file %s does not exist", filePath);
+        } catch (IOException ex) {
+            System.err.println("I/O error: " + ex);
+        }
+    }
 
 }
